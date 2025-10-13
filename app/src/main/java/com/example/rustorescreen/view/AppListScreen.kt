3834b9de0b50597bd7c1875e1a7ac743501de719
RedFeuer <danilabespyatov@gmail.com>
2025-10-13 20:03:29 +0300

    IMPROVED: previous func was depricated

--- app/src/main/java/com/example/rustorescreen/view/AppListScreen.kt
+++ app/src/main/java/com/example/rustorescreen/view/AppListScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -29,7 +30,7 @@ fun AppListScreen(
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(apps, key = {it.id}) { app ->
             AppRow(app = app, onClick = { onAppClick(app.id) } )
-            Divider()
+            HorizontalDivider()
         }
     }
 }
