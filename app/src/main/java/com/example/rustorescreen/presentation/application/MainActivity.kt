e96321160644b8774978bef255c79d44fe21d1ef
RedFeuer <danilabespyatov@gmail.com>
2025-10-16 20:52:13 +0300

    FIXED: import for Navigation

--- app/src/main/java/com/example/rustorescreen/presentation/application/MainActivity.kt
+++ app/src/main/java/com/example/rustorescreen/presentation/application/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.rustorescreen.presentation.ui.AppRoot
+import com.example.rustorescreen.presentation.viewModel.AppRoot
 import com.example.rustorescreen.ui.theme.RuStoreScreenTheme
 
 class MainActivity : ComponentActivity() {
