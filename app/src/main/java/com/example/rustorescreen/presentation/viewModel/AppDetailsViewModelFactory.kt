417c29916b531f3c3a89ae8256eeb36ed649aa69
RedFeuer <danilabespyatov@gmail.com>
2025-11-05 21:52:03 +0300

    COMMENTED: as it useless now, because we using hiltViewModel with dependency injection instead of viewModel(from JetpackViewModel) with custom Factory

--- app/src/main/java/com/example/rustorescreen/presentation/viewModel/AppDetailsViewModelFactory.kt
+++ app/src/main/java/com/example/rustorescreen/presentation/viewModel/AppDetailsViewModelFactory.kt
@@ -4,12 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 // фабрика для создания экземпляров AppDetailsViewModel с параметром appId
-class AppDetailsViewModelFactory(private val appId: Int) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(AppDetailsViewModel::class.java)) { // проверяем, что запрашиваемый класс является AppDetailsViewModel
-            @Suppress("UNCHECKED_CAST") // подавляем предупреждение о небезопасном приведении типов
-            return AppDetailsViewModel(appId) as T // создаем и возвращаем экземпляр AppDetailsViewModel с переданным appId
-        }
-        throw IllegalArgumentException("Unknown ViewModel class") // выбрасываем исключение, если запрашиваемый класс неизвестен
-    }
-}
+//class AppDetailsViewModelFactory(private val appId: Int) : ViewModelProvider.Factory {
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(AppDetailsViewModel::class.java)) { // проверяем, что запрашиваемый класс является AppDetailsViewModel
+//            @Suppress("UNCHECKED_CAST") // подавляем предупреждение о небезопасном приведении типов
+//            return AppDetailsViewModel(appId) as T // создаем и возвращаем экземпляр AppDetailsViewModel с переданным appId
+//        }
+//        throw IllegalArgumentException("Unknown ViewModel class") // выбрасываем исключение, если запрашиваемый класс неизвестен
+//    }
+//}
