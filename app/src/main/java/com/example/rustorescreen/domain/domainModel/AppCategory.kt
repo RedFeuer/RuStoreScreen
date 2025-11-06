e96988e4370495417c026083763d510827f37a26
RedFeuer <danilabespyatov@gmail.com>
2025-11-06 21:50:24 +0300

    ADDED: names of categories in API to serialize

--- app/src/main/java/com/example/rustorescreen/domain/domainModel/AppCategory.kt
+++ app/src/main/java/com/example/rustorescreen/domain/domainModel/AppCategory.kt
@@ -1,6 +1,65 @@
 package com.example.rustorescreen.domain.domainModel
 
+import kotlinx.serialization.SerialName
+
 enum class AppCategory {
-    APP, GAME, PRODUCTIVITY, SOCIAL, EDUCATION, ENTERTAINMENT, MUSIC, VIDEO, PHOTOGRAPHY, HEALTH, SPORTS,
-    NEWS, BOOKS, BUSINESS, FINANCE, LIFESTYLE, TRAVEL, MAPS, FOOD, SHOPPING, UTILITIES
+    @SerialName("Приложения")
+    APP,
+
+    @SerialName("Игры")
+    GAME,
+
+    @SerialName("Производительность")
+    PRODUCTIVITY,
+
+    @SerialName("Социальные сети")
+    SOCIAL,
+
+    @SerialName("Образование")
+    EDUCATION,
+
+    @SerialName("Развлечения")
+    ENTERTAINMENT,
+
+    @SerialName("Музыка")
+    MUSIC,
+
+    @SerialName("Видео")
+    VIDEO,
+
+    @SerialName("Фотография")
+    PHOTOGRAPHY,
+
+    @SerialName("Здоровье")
+    HEALTH,
+
+    @SerialName("Спорт")
+    SPORTS,
+
+    @SerialName("Новости")
+    NEWS,
+
+    @SerialName("Книги")
+    BOOKS,
+
+    @SerialName("Бизнес")
+    BUSINESS,
+
+    @SerialName("Финансы")
+    FINANCE,
+
+    @SerialName("Путешествия")
+    TRAVEL,
+
+    @SerialName("Карты")
+    MAPS,
+
+    @SerialName("Еда")
+    FOOD,
+
+    @SerialName("Покупки")
+    SHOPPING,
+
+    @SerialName("Утилиты")
+    UTILITIES
 }
