c787afef0cbe38a4d365430ceb182f4a8dfd5ee1
RedFeuer <danilabespyatov@gmail.com>
2025-11-07 18:40:57 +0300

    IMPROVED: make invoke suspend

--- app/src/main/java/com/example/rustorescreen/domain/useCase/GetAppListUseCase.kt
+++ app/src/main/java/com/example/rustorescreen/domain/useCase/GetAppListUseCase.kt
@@ -7,14 +7,8 @@ import com.example.rustorescreen.domain.repositoryInterface.AppListRepository
 class GetAppListUseCase(
     private val appRepository: AppListRepository // Dependency injection of the AppRepository
 ) {
-    operator fun invoke(): List<AppDetails> {
+    suspend operator fun invoke(): List<AppDetails> {
         val apps: List<AppDetails> = appRepository.get()// Fetches the App data from the repository when called
-
-        for (app in apps) {
-            if (app.id < 0) {
-                throw IllegalArgumentException("App with name: ${app.name} has id: ${app.id}. But id must be non-negative.")
-            }
-        }
 
         return apps
     }
