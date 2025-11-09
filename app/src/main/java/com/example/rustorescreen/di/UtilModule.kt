6197a529e2e843e46c5b01ccff8e298af9c1a994
RedFeuer <danilabespyatov@gmail.com>
2025-11-09 15:49:31 +0300

    ADDED: documentation

--- app/src/main/java/com/example/rustorescreen/di/UtilModule.kt
+++ app/src/main/java/com/example/rustorescreen/di/UtilModule.kt
@@ -9,9 +9,24 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 
+/**
+ * Hilt\-модуль, предоставляющий утилитарные зависимости приложения.
+ *
+ * Установлен в [SingletonComponent], поэтому зависимости из этого модуля
+ * живут в пределах жизненного цикла приложения (Singleton).
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class UtilModule {
+    /**
+     * Привязывает реализацию [LoggerImpl] к интерфейсу [Logger].
+     *
+     * Аннотация @Singleton указывает, что будет создан один экземпляр
+     * [LoggerImpl] на весь срок работы приложения.
+     *
+     * @param impl конкретная реализация, предоставляемая Hilt
+     * @return интерфейс, используемый зависимостями
+     */
     @Binds
     @Singleton
     abstract fun bindLogger(impl: LoggerImpl): Logger
