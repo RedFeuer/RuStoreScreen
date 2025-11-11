54645aeb4d4605c6047229c4c14c620668d9cf46
RedFeuer <danilabespyatov@gmail.com>
2025-11-11 20:06:44 +0300

    IMPROVED: added appListDao to support getting AppList from DB

--- app/src/main/java/com/example/rustorescreen/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/rustorescreen/data/local/AppDatabase.kt
@@ -6,11 +6,13 @@ import androidx.room.TypeConverters
 
 @Database(
     entities = [AppDetailsEntity::class],
-    version = 1,
+    version = 2,
 )
 @TypeConverters(CategoryConverter::class, ScreenshotsListConverter::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun appDetailsDao(): AppDetailsDao
+
+    abstract fun appListDao(): AppListDao
 
     companion object {
         const val DATABASE_NAME = "app_database"
