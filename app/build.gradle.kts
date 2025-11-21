b7e661a4ef8c18e77b4e96be755893db3b2c2c0a
RedFeuer <danilabespyatov@gmail.com>
2025-11-21 20:02:11 +0300

    ADDED: room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.8.3")
     implementation("androidx.room:room-ktx:2.8.3")
     kapt("androidx.room:room-compiler:2.8.3")
+    implementation(libs.room.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
