0adb2070f8bd7e4346611a9e0ec25855140b7189
RedFeuer <danilabespyatov@gmail.com>
2025-12-12 21:08:08 +0300

    DELETED: reinstall as it's have the same logic as the install func

--- app/src/main/java/com/example/rustorescreen/presentation/ui/InstallActions.kt
+++ app/src/main/java/com/example/rustorescreen/presentation/ui/InstallActions.kt
@@ -3,6 +3,5 @@ package com.example.rustorescreen.presentation.ui
 data class InstallActions(
     val install: () -> Unit,
     val uninstall: () -> Unit,
-    val reinstall: () -> Unit,
     val open: () -> Unit,
 )
