cdee6bcd041a6788a2ea205ce17753869baca5bf
RedFeuer <danilabespyatov@gmail.com>
2026-02-03 18:50:17 +0300

    IMPROVE-cancel installation button: added new comments

--- app/src/main/java/com/example/rustorescreen/domain/domainModel/InstallStatus.kt
+++ app/src/main/java/com/example/rustorescreen/domain/domainModel/InstallStatus.kt
@@ -1,6 +1,7 @@
 package com.example.rustorescreen.domain.domainModel
 
 sealed interface InstallStatus {
+    /* установка приложения */
     data object Idle: InstallStatus // = Uninstalled = состояние по умолчанию
     data object InstallPrepared: InstallStatus
     data object InstallStarted: InstallStatus
@@ -8,8 +9,7 @@ sealed interface InstallStatus {
     data object Installed: InstallStatus
     data class InstallError(val error: Throwable): InstallStatus
 
-    /* TODO:
-    *   1) добавить возможность удаления приложения*/
+    /* удаление приложения */
     data object UninstallPrepared: InstallStatus
     data object Uninstalling: InstallStatus
     data class UninstallError(val error: Throwable): InstallStatus
